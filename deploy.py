84651c38697339ef9c4c797525bf7395ab09b106
Priti Yadav <pritiyadav888.com>
2023-05-18 15:26:53 -0700

    deploying to heroku

--- deploy.py
+++ deploy.py
@@ -14,13 +14,13 @@ def run_cmd(cmd):
     stdout, stderr = process.communicate()
     return stdout.decode('utf-8'), stderr.decode('utf-8'), process.returncode
 
-def create_requirements():
-    stdout, stderr, exitcode = run_cmd("pip freeze > requirements.txt")
-    if exitcode == 0:
-        logging.info("requirements.txt has been successfully created/updated")
-    else:
-        logging.error(f"An error occurred during requirements.txt creation: {stderr}")
-        return exitcode
+# def create_requirements():
+#     stdout, stderr, exitcode = run_cmd("pip freeze > requirements.txt")
+#     if exitcode == 0:
+#         logging.info("requirements.txt has been successfully created/updated")
+#     else:
+#         logging.error(f"An error occurred during requirements.txt creation: {stderr}")
+#         return exitcode
 
 def create_procfile():
     if not os.path.isfile('Procfile'):
@@ -60,7 +60,7 @@ def main():
     run_cmd("python3 -m venv venv")
     run_cmd("source venv/bin/activate")
     run_cmd("pip install -r requirements.txt")
-    create_requirements()
+    # create_requirements()
     create_procfile()
     create_heroku_app(heroku_app_name)
     push_to_heroku(heroku_app_name)
