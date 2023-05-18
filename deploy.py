bd10830835aaf118170b77c511ca7a24d5d70b37
Priti Yadav <pritiyadav888.com>
2023-05-18 14:57:00 -0700

    deploying to heroku

--- deploy.py
+++ deploy.py
@@ -2,6 +2,14 @@ import os
 import subprocess
 from getpass import getpass
 import openai
+import logging
+
+# Set up logging
+logging.basicConfig(filename='deploy.log', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+logging.getLogger('').addHandler(console)
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def run_cmd(cmd):
@@ -10,9 +18,7 @@ def run_cmd(cmd):
     return stdout.decode('utf-8'), stderr.decode('utf-8'), process.returncode
 
 def resolve_errors_with_gpt(error_message):
-    # try to find a solution up to 10 times
     for attempt in range(10):
-        # ask GPT-3 for advice on how to resolve the error
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -26,50 +32,42 @@ def resolve_errors_with_gpt(error_message):
                 },
             ],
         )
-
         advice = response.choices[0].message['content']
-
-        print(f"GPT-3 advice: {advice}")
-
-        # apply the advice from GPT-3
+        logging.info(f"GPT-3 advice: {advice}")
         stdout, stderr, exitcode = run_cmd(advice)
 
         if exitcode == 0:
-            # if the advice worked, break the loop
-            print("The advice worked. Continuing with the deployment...")
+            logging.info("The advice worked. Continuing with the deployment...")
             return
         else:
-            # if the advice didn't work, let GPT-3 know and ask for new advice
-            print(f"The advice didn't work. Error: {stderr}")
+            logging.error(f"The advice didn't work. Error: {stderr}")
             error_message = f"The advice didn't work. Here's the new error message: {stderr}"
+    logging.error("GPT-3 was unable to resolve the error after 10 attempts.")
 
-    print("GPT-3 was unable to resolve the error after 10 attempts.")
 def check_git_changes():
     stdout, stderr, exitcode = run_cmd("git status")
     if "nothing to commit, working tree clean" in stdout:
-        print("There are no changes to commit.")
+        logging.info("There are no changes to commit.")
         return False
     else:
-        print("There are changes to commit.")
+        logging.info("There are changes to commit.")
         return True
 
 def create_requirements():
     stdout, stderr, exitcode = run_cmd("pipreqs ./ --force")
     if exitcode == 0:
-        print("requirements.txt has been successfully created/updated")
+        logging.info("requirements.txt has been successfully created/updated")
     else:
-        print(f"An error occurred during requirements.txt creation: {stderr}")
+        logging.error(f"An error occurred during requirements.txt creation: {stderr}")
         return exitcode
-
 
 def create_procfile():
     if not os.path.isfile('Procfile'):
         with open('Procfile', 'w') as f:
             f.write('web: python app.py')
-        print("Procfile has been successfully created")
+        logging.info("Procfile has been successfully created")
     else:
-        print("Procfile already exists")
-
+        logging.info("Procfile already exists")
 
 def push_to_heroku(heroku_app_name):
     commands = [
@@ -83,18 +81,16 @@ def push_to_heroku(heroku_app_name):
     for cmd in commands:
         stdout, stderr, exitcode = run_cmd(cmd)
         if exitcode != 0:
-            print(f"An error occurred during executing '{cmd}': {stderr}")
+            logging.error(f"An error occurred during executing '{cmd}': {stderr}")
             resolve_errors_with_gpt(stderr)
-            # if still unresolved, return from the function
             if exitcode != 0:
                 return exitcode
-        print(stdout)
-
-
+        logging.info(stdout)
 
 def create_heroku_app(heroku_app_name):
     stdout, stderr, exitcode = run_cmd(f"heroku create {heroku_app_name}")
     if exitcode == 0:
+        logging.info(f"Heroku app create")
         print(f"Heroku app '{heroku_app_name}' has been successfully created")
     else:
         print(f"An error occurred during Heroku app creation: {stderr}")
